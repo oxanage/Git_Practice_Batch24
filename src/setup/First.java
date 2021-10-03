7c1076e0eca1e91c6c84b315eb1e09aa82182d69
oxanage <oxanage@yahoo.com>
2021-10-03 11:40:48 -0500

    updated first file

--- src/setup/First.java
+++ src/setup/First.java
@@ -5,9 +5,6 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        // This is a new line
-        // I want to create a history
-        // The text doesn"t matter
-
+      // This is a new line
     }
 }
